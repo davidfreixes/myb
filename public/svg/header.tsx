034d8fb62a57aa34c3c6a906de242f3eaeeaa86b
David Freixes <dfis1996@gmail.com>
2024-12-28 18:20:24 +0100

    fix: icons bars

--- public/svg/header.tsx
+++ public/svg/header.tsx
@@ -1,30 +1,19 @@
-import React from "react";
-
-interface IconProps {
-  className?: string;
-}
-
-const headerIcon = (path: JSX.Element): React.FC<IconProps> => {
-  return ({ className = "h-7 w-7" }) => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth="1.5"
-        stroke={"white"}
-        className={className}
-      >
-        {path}
-      </svg>
-    );
-  };
+export const Bars = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="white"
+      className="h-7 w-7 mr-2"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
 };
 
-export const FaceSmile = headerIcon(
-  <path
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-  />
-);
