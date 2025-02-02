3ff42b1e3aa509ea4ecd21cbc1c15a50b5914042
David Freixes <dfis1996@gmail.com>
2025-02-02 23:41:46 +0100

    fix: pause google recaptcha

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import "@mantine/core/styles.css";
 import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import { I18nextProvider } from "react-i18next";
 import SEO from "../next-seo.config";
 import "../utils/i18n";
@@ -19,17 +18,17 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#000000" />
       </Head>
       <NextSeo {...SEO} />
-      <GoogleReCaptchaProvider
+      {/* <GoogleReCaptchaProvider
         reCaptchaKey={process.env.NEXT_PUBLIC_CAPTCHA_CLIENT}
         language="es"
         useEnterprise={true}
-      >
+      > */}
         <MantineProvider>
           <I18nextProvider i18n={i18n}>
             <Component {...pageProps} />
           </I18nextProvider>
         </MantineProvider>
-      </GoogleReCaptchaProvider>
+      {/* </GoogleReCaptchaProvider> */}
     </>
   );
 }
