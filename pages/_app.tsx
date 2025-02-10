4d983e912b6afb508b6d40e2a897b74b6c323fed
David Freixes <dfis1996@gmail.com>
2025-02-11 00:34:39 +0100

    css: delete SEO From app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
-import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { I18nextProvider } from "react-i18next";
-import SEO from "../next-seo.config";
 import "../utils/i18n";
 import i18n from "../utils/i18n";
 
@@ -17,7 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="theme-color" content="#000000" />
       </Head>
-      <NextSeo {...SEO} />
+      {/* <NextSeo {...SEO} /> */}
       {/* <GoogleReCaptchaProvider
         reCaptchaKey={process.env.NEXT_PUBLIC_CAPTCHA_CLIENT}
         language="es"
