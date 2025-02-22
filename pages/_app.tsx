84a3b0b50fbc2b320c5ee96b46b79339ecc56a98
David Freixes <dfis1996@gmail.com>
2025-02-22 11:59:24 +0100

    feat: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { I18nextProvider } from "react-i18next";
 import "../utils/i18n";
 import i18n from "../utils/i18n";
@@ -21,6 +22,18 @@ export default function App({ Component, pageProps }: AppProps) {
         language="es"
         useEnterprise={true}
       > */}
+      {/* Google Analytics */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=G-3HH99L60F4`}
+      />
+      <Script id="google-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date()); gtag('config', 'G-3HH99L60F4');
+          `}
+      </Script>
       <MantineProvider>
         <I18nextProvider i18n={i18n}>
           <Component {...pageProps} />
